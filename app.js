53fc4030d9485e0c081ed40c4798bb0c696debde
SangYong-Jeong <mmaf@naver.com>
2021-10-21 11:03:44 +0900

    deploy

--- app.js
+++ app.js
@@ -4,5 +4,4 @@ const path = require('path')
 
 app.use('/', express.static(path.join(__dirname, './dist')))
 
-console.log(path.join(__dirname, '/dist'))
 app.listen(3200, () => { console.log('http://127.0.0.1:3200') })
